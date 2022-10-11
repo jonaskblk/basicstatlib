7b957b1b47e1a373f12415f659f8ad1aa19a1f13
Jonas Kubelke <jonas.kubelke@gmail.com>
2022-10-11 21:35:39 +0200

    implemented add method and test

--- basicstatlib/linearalgebra/test_vector.py
+++ basicstatlib/linearalgebra/test_vector.py
@@ -2,8 +2,21 @@ import pytest
 
 from .. linearalgebra.vector import Vector
 
-def test_create_int_vector_success() -> None:
-    values = [0, 1 ,3]
+@pytest.mark.parametrize('values', [
+    (
+        [0, 1 ,3]
+    ),
+    (
+        [0, 0, 0]
+    ),
+    (
+        [-1, -2, -3]
+    ),
+    (
+        [-1, 2, 33]
+    ),
+])
+def test_create_int_vector_success(values) -> None:
     vector = Vector(values)
     assert vector.components.__eq__(values)
 
@@ -21,3 +34,10 @@ def test_create_empty_vector_fail() -> None:
     with pytest.raises(Exception):
         values = []
         Vector(values)
+
+def test_add_two_vectors_success() -> None:
+    vector_a = Vector([1, 2, 3])
+    vector_b = Vector([4, 5, 6])
+    vector_c = Vector([5, 7, 9])
+    vector_a.add(vector_b)
+    assert vector_a.components.__eq__(vector_c.components)
--- basicstatlib/linearalgebra/vector.py
+++ basicstatlib/linearalgebra/vector.py
@@ -17,3 +17,8 @@ class Vector:
     def set_value_of_component(self, index: int, value: float) -> float:
         assert index > 0, "Index must be greater then 0"
         self.components[index - 1] = value
+
+    def add(self, vector_b: 'Vector'):
+        vector_a = Vector(self.components)
+        assert len(vector_a.components) == len(vector_b.components), "Vectors must be of the same length"
+        self.components = [a_i + b_i for a_i, b_i in zip(vector_a.components, vector_b.components)]
