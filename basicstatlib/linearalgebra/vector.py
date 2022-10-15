c76dff7fca0ce0a28aef69eb694faeb1cdad5aac
Jonas Kubelke <jonas.kubelke@gmail.com>
2022-10-15 23:35:23 +0200

    overriding necessary base class methods

--- basicstatlib/linearalgebra/vector.py
+++ basicstatlib/linearalgebra/vector.py
@@ -2,28 +2,46 @@ class Vector:
     def __init__(self, components: list) -> None:
         _VectorValidator.guard_components_not_empty(components)
         _VectorValidator.guard_components_of_type_float_or_int(components)
-        self.components = components
+        self.__components = components
+
+    def __eq__(self, other: 'object') -> bool:
+        _VectorValidator.guard_is_of_type_vector(other)
+        if (self.__components.__eq__(other.get_components())):
+            return True
+        else:
+            return False
+
+    def __str__(self) -> str:
+        return self.__components.__str__()
+
+    def __repr__(self) -> str:
+        return f"Vector({self.__str__()})"
+
+    def get_components(self) -> list:
+        return self.__components
 
     def get_component_by_index(self, index: int) -> float:
         _VectorValidator.guard_index_greater_then_zero(index)
-        return self.components[index - 1]
+        return self.__components[index - 1]
 
     def set_value_of_component(self, index: int, value: float) -> float:
         _VectorValidator.guard_index_greater_then_zero(index)
-        self.components[index - 1] = value
+        self.__components[index - 1] = value
 
     def add(self, vector_b: 'Vector'):
-        vector_a = Vector(self.components)
+        vector_a = Vector(self.__components)
         _VectorValidator.guard_equal_length_of_components(vector_a, vector_b)
-        self.components = [a_i + b_i for a_i, b_i in zip(vector_a.components, vector_b.components)]
+        self.__components = [a_i + b_i for a_i, b_i in zip(vector_a.get_components(), vector_b.get_components())]
 
     def subtract(self, vector_b: 'Vector'):
-        vector_a = Vector(self.components)
+        vector_a = Vector(self.__components)
         _VectorValidator.guard_equal_length_of_components(vector_a, vector_b)
-        self.components = [a_i - b_i for a_i, b_i in zip(vector_a.components, vector_b.components)]
+        self.__components = [a_i - b_i for a_i, b_i in zip(vector_a.get_components(), vector_b.get_components())]
 
     def multiply_with_scalar(self, scalar: float) -> None:
-        pass
+        _VectorValidator.guard_is_a_number(scalar)
+        vector = Vector(self.__components)
+        self.__components = [index * scalar for index in vector.get_components()]
 
     def sum_of_squares(self) -> float:
         pass
@@ -61,11 +79,25 @@ class _VectorValidator:
             raise ValueError("Components must be of type float or int")
 
     @staticmethod
-    def guard_index_greater_then_zero(index):
+    def guard_is_of_type_vector(object_to_check: object) -> None:
+        if not (isinstance(object_to_check, Vector)):
+            raise ValueError("Object must be of type vector")
+
+    @staticmethod
+    def guard_index_greater_then_zero(index) -> None:
         if not index > 0:
             raise ValueError("Index must be greater then 0")
 
     @staticmethod
-    def guard_equal_length_of_components(vector_a: 'Vector', vector_b: 'Vector'):
-        if not len(vector_a.components) == len(vector_b.components):
+    def guard_equal_length_of_components(vector_a: Vector, vector_b: Vector) -> None:
+        if not len(vector_a.get_components()) == len(vector_b.get_components()):
             raise Exception("Vectors must be of the same length")
+    
+    @staticmethod
+    def guard_is_a_number(input) -> None:
+        if (isinstance(input, float)):
+            pass
+        elif (isinstance(input, int)):
+            pass
+        else:
+            raise ValueError("Input must be of type float or int")
--- test_basicstatlib/test_linearalgebra/test_vector.py
+++ test_basicstatlib/test_linearalgebra/test_vector.py
@@ -17,7 +17,24 @@ from basicstatlib.linearalgebra.vector import Vector
 ])
 def test_create_int_vector_success(values) -> None:
     vector = Vector(values)
-    assert vector.components.__eq__(values)
+    assert vector.get_components.__eq__(values)
+
+def test___eq__() -> None:
+    vector_a = Vector([5, 7, 9])
+    vector_b = Vector([5, 7, 9])
+    vector_c = Vector([2, 3, 5])
+    assert vector_a.__eq__(vector_b)
+    assert not vector_a.__eq__(vector_c)
+
+def test___str__() -> None:
+    vector = Vector([5, 7, 9])
+    str_of_vector = "[5, 7, 9]"
+    assert vector.__str__().__eq__(str_of_vector)
+
+def test___repr__() -> None:
+    vector = Vector([5, 7, 9])
+    repr_of_vector = "Vector([5, 7, 9])"
+    assert vector.__repr__().__eq__(repr_of_vector)
 
 @pytest.mark.parametrize("values", [
     (
@@ -35,7 +52,7 @@ def test_create_int_vector_success(values) -> None:
 ])
 def test_create_float_vector_success(values) -> None:
     vector = Vector(values)
-    assert vector.components.__eq__(values)
+    assert vector.get_components.__eq__(values)
 
 @pytest.mark.parametrize("values", [
     (
@@ -82,18 +99,25 @@ def test_set_value_of_component() -> None:
     index_of_component = 2
     new_value = 3.0
     vector_a.set_value_of_component(index_of_component, new_value)
-    assert vector_a.components[index_of_component - 1] == new_value
+    assert vector_a.get_component_by_index(index_of_component) == new_value
 
 def test_add_two_vectors_success() -> None:
     vector_a = Vector([1, 2, 3])
     vector_b = Vector([4, 5, 6])
     vector_c = Vector([5, 7, 9])
     vector_a.add(vector_b)
-    assert vector_a.components.__eq__(vector_c.components)
+    assert vector_a.__eq__(vector_c)
 
 def test_subtract_two_vectors_success() -> None:
     vector_a = Vector([5, 7, 9])
     vector_b = Vector([1, 2, 3])
     vector_c = Vector([4, 5, 6])
     vector_a.subtract(vector_b)
-    assert vector_a.components.__eq__(vector_c.components)
+    assert vector_a.__eq__(vector_c)
+
+def test_multiply_with_scalar_success() -> None:
+    vector_a = Vector([5, 7, 9])
+    vector_b = Vector([10, 14, 18])
+    scalar = 2
+    vector_a.multiply_with_scalar(scalar)
+    assert vector_a.__eq__(vector_b)
