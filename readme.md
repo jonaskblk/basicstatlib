5e54ec03693ecc92885e518c614b92656a0b85b1
Jonas Kubelke <jonas.kubelke@gmail.com>
2022-10-17 20:51:38 +0200

    edited setup file

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Run Unittests
 
 'python setup.py test'
+
+<https://stackoverflow.com/questions/42258964/build-conda-package-from-local-python-package>
--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='qlib',
     packages=find_packages(include=['qlib']),
-    version='0.1.0',
-    description='A basic statistic library',
+    version='0.0.1',
+    description='A basic data science library',
     author='Jonas Kubelke',
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner==2.11.1'],
     tests_require=['pytest==7.1.1'],
     test_suite='test_qlib',
+    package_dir={'':'qlib'}
 )
