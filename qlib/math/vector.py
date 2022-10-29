bc7890a67505a9952f9189bc7e15b56a2e3119f7
Jonas Kubelke <jonas.kubelke@gmail.com>
2022-10-29 15:33:00 +0200

    completed implementation of vector

--- qlib/math/vector.py
+++ qlib/math/vector.py
@@ -1,15 +1,9 @@
-"""Contains definition for type Vector and VectorValidator.
-"""
+import math
 
 from .common import CommonValidator
 
 
 class Vector:
-    """Defines instances of type vector.
-
-    Returns:
-        _type_: vector
-    """
 
     def __init__(self, components: list) -> None:
         self.__vector_validator = VectorValidator()
@@ -32,100 +26,60 @@ class Vector:
         return f"Vector({self.__components.__str__()})"
 
     def get_components(self) -> list:
-        """Returns the components of a vector instance as a list.
-        """
         return self.__components
 
     def get_component_by_index(self, index: int) -> float:
-        """Returns the value of a single component specified by a given index.
-
-        Args:
-            index (int): Index of component greater than 0
-
-        Returns:
-            float: Value of component at given index
-        """
         self.__vector_validator.guard_index_greater_then_zero(index)
         return self.__components[index - 1]
 
     def set_value_of_component(self, index: int, value: float) -> None:  # type: ignore
-        """Sets the value of a single component specified by a given index.
-
-        Args:
-            index (int): Index of the manipulated component greatar than 0
-            value (float): New Value of the component
-        """
         self.__vector_validator.guard_index_greater_then_zero(index)
 
         self.__components[index - 1] = value
 
     def get_number_of_components(self) -> int:
-        """Gets the number components.
-
-        Returns:
-            int: Number of components.
-        """
         return int(len(self.__components))
 
     def add(self, vector_b: 'Vector') -> None:
-        """Adds up two vectors componentwise.
-
-        Args:
-            vector_b (Vector): Vector that should be added
-        """
-        vector_a = Vector(self.__components)
         self.__vector_validator.guard_equal_length_of_components(
-            vector_a, vector_b)
+            self, vector_b)
         new_components = [
-            a_i + b_i for a_i, b_i in zip(vector_a.get_components(), vector_b.get_components())]
+            a_i + b_i for a_i, b_i in zip(self.get_components(), vector_b.get_components())]
         self.__set_components(new_components)
 
     def subtract(self, vector_b: 'Vector') -> None:
-        """Subtracts one vector to from the other componentwise.
-
-        Args:
-            vector_b (Vector): Vector that should be subtracted
-        """
-        vector_a = Vector(self.__components)
         self.__vector_validator.guard_equal_length_of_components(
-            vector_a, vector_b)
+            self, vector_b)
         new_components = [
-            a_i - b_i for a_i, b_i in zip(vector_a.get_components(), vector_b.get_components())]
+            a_i - b_i for a_i, b_i in zip(self.get_components(), vector_b.get_components())]
         self.__set_components(new_components)
 
     def multiply_with_scalar(self, scalar: float) -> None:
-        """Multiplys a vector componentwise with a given scalar.
-
-        Args:
-            scalar (float): Number which the components get multiplied with
-        """
         self.__vector_validator.guard_is_a_number(scalar)
         vector = Vector(self.__components)
         new_components = [index * scalar for index in vector.get_components()]
         self.__set_components(new_components)
 
+    def dot_product(self, vector_b: 'Vector') -> float:
+        self.__vector_validator.guard_equal_length_of_components(
+            self, vector_b)
+
+        return sum(a_i * b_i for a_i,
+                   b_i in zip(self.get_components(), vector_b.get_components()))
+
     def sum_of_squares(self) -> float:  # type: ignore
-        pass
+        return self.dot_product(self)
 
     def magnitude(self) -> float:  # type: ignore
-        pass
+        return math.sqrt(self.sum_of_squares())
 
-    def distance(self, vector_b: 'Vector') -> float:  # type: ignore
-        pass
-
-    def squared_distance(self, vector_b: 'Vector') -> float:  # type: ignore
-        pass
+    def distance_to(self, vector_b: 'Vector') -> float:  # type: ignore
+        vector_a = self
+        vector_a.subtract(vector_b)
+        return vector_a.magnitude()
 
     @ staticmethod
     def sum_up_vectors(vectors: list['Vector']) -> 'Vector':  # type: ignore
-        """Sums up a list of vectors componentwise.
-
-        Args:
-            vectors (list[&#39;Vector&#39;]): Vector-list of interest
-
-        Returns:
-            Vector: Summed up vectors as a new vector
-        """
         vector_validator = VectorValidator()
         vector_validator.guard_list_of_vectors_not_empty(vectors)
         vector_validator.guard_equal_length_of_components_in_list(vectors)
@@ -142,14 +96,6 @@ class Vector:
 
     @ staticmethod
     def mean_of_vectors(vectors: list['Vector']) -> 'Vector':  # type: ignore
-        """Calculates the mean of a list of vectors componentwise.
-
-        Args:
-            vectors (list[&#39;Vector&#39;]): Vector-list of interests
-
-        Returns:
-            Vector: Mean of vectors as a new vector
-        """
         vector_validator = VectorValidator()
         vector_validator.guard_list_of_vectors_not_empty(vectors)
         vector_validator.guard_equal_length_of_components_in_list(vectors)
@@ -161,6 +107,7 @@ class Vector:
 
 
 class VectorValidator:
+
     def __init__(self) -> None:
         self.common_validator = CommonValidator()
 
